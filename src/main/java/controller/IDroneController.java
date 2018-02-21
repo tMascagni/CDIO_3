2919cf74701b22847dc940f5ef1ae2ae2d5960eb
InAbsentia <absentia@protonmail.ch>
2018-02-21 11:52:13 +0100

    Update DroneController. Needs to be tested.

--- src/main/java/controller/IDroneController.java
+++ src/main/java/controller/IDroneController.java
@@ -12,6 +12,16 @@ public interface IDroneController {
 
     void hoverDrone(int timeMillis) throws DroneControllerException;
 
+    void searchRotation() throws DroneControllerException;
+
+    void flyForward(int distanceMilli) throws DroneControllerException;
+    void flyBackward(int distanceMilli) throws DroneControllerException;
+
+    void flyUp(int distanceMilli) throws DroneControllerException;
+    void flyRight(int distanceMilli) throws DroneControllerException;
+    void flyLeft(int distanceMilli) throws DroneControllerException;
+    void flyDown(int distanceMilli) throws DroneControllerException;
+
     void resetDrone() throws DroneControllerException;
     IARDrone getDrone() throws DroneControllerException;
 }
