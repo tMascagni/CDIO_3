9958e14ba9445114119df079e9327e2a3ada3570
InAbsentia <absentia@protonmail.ch>
2018-03-03 18:06:47 +0100

    Revised main methods.

--- src/main/java/cdio/main/Main.java
+++ src/main/java/cdio/main/Main.java
@@ -2,8 +2,6 @@ package cdio.main;
 
 import cdio.controller.DroneController;
 import cdio.controller.interfaces.IDroneController;
-import cdio.ui.MainFrame;
-import cdio.ui.interfaces.MessageListener;
 
 import java.awt.*;
 
@@ -11,31 +9,29 @@ public final class Main {
 
     private final static IDroneController droneController = DroneController.getInstance();
 
-    public static void main(String[] args) throws IDroneController.DroneControllerException {
+    public static void main(String[] args) {
 
-       // MainFrame mainFrame = new MainFrame(droneController);
-        droneController.setMessageListener(new MessageListener() {
-            @Override
-            public void messageEventOccurred(Object obj, String text) {
+        droneController.setMessageListener((obj, text) -> {
 
-            }
         });
-
-        droneController.startDrone();
-        droneController.initDrone();
-
-        droneController.takeOffDrone();
-
-        droneController.hoverDrone(10000);
-
-        //droneController.searchRotation();
-
-        droneController.landDrone();
-        droneController.stopDrone();
 
         /* ######### TEST ######### */
         EventQueue.invokeLater(() -> {
+            try {
+                droneController.startDrone();
+                droneController.initDrone();
 
+                droneController.takeOffDrone();
+
+                droneController.hoverDrone(10000);
+
+                //droneController.searchRotation();
+
+                droneController.landDrone();
+                droneController.stopDrone();
+            } catch (IDroneController.DroneControllerException e) {
+                e.printStackTrace();
+            }
         });
         /* ######### TEST ######### */
 
--- src/main/java/cdio/main/MainGUI.java
+++ src/main/java/cdio/main/MainGUI.java
@@ -4,15 +4,33 @@ import cdio.controller.DroneController;
 import cdio.controller.interfaces.IDroneController;
 import cdio.ui.MainFrame;
 
+import java.awt.*;
+
 public final class MainGUI {
 
     private static final IDroneController droneController = DroneController.getInstance();
 
-    public static void main(String[] args) throws IDroneController.DroneControllerException {
+    public static void main(String[] args) {
         MainFrame mainFrame = new MainFrame(droneController);
         droneController.setMessageListener(mainFrame);
 
-        droneController.takeOffDrone();
+        /* ######### TEST ######### */
+        EventQueue.invokeLater(() -> {
+            try {
+                droneController.startDrone();
+                droneController.initDrone();
+
+                droneController.takeOffDrone();
+
+                droneController.hoverDrone(8000);
+
+                droneController.landDrone();
+                droneController.stopDrone();
+            } catch (IDroneController.DroneControllerException e) {
+                e.printStackTrace();
+            }
+        });
+        /* ######### TEST ######### */
     }
 
 }
