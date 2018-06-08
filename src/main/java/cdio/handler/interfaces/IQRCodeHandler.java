b19610783b2eb4da3ba47b18b34ec9714f47f420
piill <pil@disroot.org>
2018-06-08 10:54:43 +0200

    Tilføjede lettere interface til qrHandler (interface)

--- src/main/java/cdio/handler/interfaces/IQRCodeHandler.java
+++ src/main/java/cdio/handler/interfaces/IQRCodeHandler.java
@@ -15,6 +15,10 @@ public interface IQRCodeHandler {
     BufferedImage getImageRemote(String url) throws QRCodeHandlerException;
     void saveImage(String imageUrl, String destinationFile) throws QRCodeHandlerException;
 
+    boolean detectQR(QRImg ret, IDroneCommander droneCommander);
+
+    boolean detectReadQR(QRImg ret, IDroneCommander droneCommander);
+
     class QRCodeHandlerException extends Exception {
 
         public QRCodeHandlerException(String msg) {
